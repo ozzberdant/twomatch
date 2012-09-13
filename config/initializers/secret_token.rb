47f3d9f6a51719ed9e8a2a6591da741e467240db
Richard Lepe <richard.lepe@gmail.com>
2012-09-13 01:05:27 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twomatch::Application.config.secret_token = 'f2fcd36b7fba3b5b842d95156bbe733266db23d90ad9c0befef0d6e7fa0f005cc4c66d40aaf7addc405082829b73daeb2003ff30447af4c28d119915b1d2fac2'
