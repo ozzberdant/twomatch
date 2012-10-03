f3474c3f9bbf20751e89d706a0777d3125a0ed9a
Richard Lepe <richard.lepe@gmail.com>
2012-10-03 01:33:55 -0300

    validate user

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Twomatch::Application.routes.draw do
 
-  get "users/new"
-
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new' 
  
- resources :instalacions
-
+  resources :instalacions
   resources :recintos
+  resources :users
 
   get "welcome/index"
 
